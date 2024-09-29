e64e69495641c511addc76b46640d294c9af1bd0
Cristian Armesto <cristianarmestotejedo@gmail.com>
2024-09-29 17:54:51 +0200

    feat: Mongo included

---
+++ errors/errors.go
@@ -0,0 +1,27 @@
+package errors
+
+import "fmt"
+
+type ConnectionError struct {
+	Db string
+}
+
+func (e *ConnectionError) Error() string {
+	return fmt.Sprintf("Connection refused from %s", e.Db)
+}
+
+type AlreadyExistError struct {
+	Message string
+}
+
+func (e *AlreadyExistError) Error() string {
+	return e.Message
+}
+
+type NotExistError struct {
+	Message string
+}
+
+func (e *NotExistError) Error() string {
+	return e.Message
+}
